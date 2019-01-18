c5307cf234492b3af3680466a67a5023f527f8d8
guyts <guytsror@gmail.com>
2019-01-18 10:51:06 -0500

    CPU version works
    Note that the directory needs to be shallow - too many folders in, it might generate erross

--- transfer-learning/deeplabcut-trial-cpu.py
+++ transfer-learning/deeplabcut-trial-cpu.py
@@ -18,3 +18,9 @@ deeplabcut.check_labels(path_config_file)
 deeplabcut.create_training_dataset(path_config_file)
 deeplabcut.train_network(path_config_file, shuffle=1)
 deeplabcut.evaluate_network(path_config_file,plotting=True)
+
+# creating the video path
+videofile_path = '/Reaching-Mackenzie-2018-08-30/videos/MovieS2_Perturbation_noLaser_compressed.avi'
+print("Start Analyzing the video!")
+deeplabcut.analyze_videos(path_config_file,[videofile_path])
+deeplabcut.create_labeled_video(path_config_file,[videofile_path])
