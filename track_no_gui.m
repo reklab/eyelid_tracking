744460af0a43aa3fd31ebf8a69c0864246dc6b6b
guyts <guytsror@gmail.com>
2019-04-05 16:26:26 -0400

    setting up alternative access
    instead of gui-only

---
+++ track_no_gui.m
@@ -0,0 +1,7 @@
+% This code can be used and manipulated to load the process without using
+% the GUI. 
+% Instead, create or edit parameter files in advance with all the required
+% parameters, and load using the provided function, to feed into the
+% tracking function, eyelid_tracking_gui.
+
+
