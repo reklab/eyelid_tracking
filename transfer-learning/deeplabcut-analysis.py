c92d386e955096f54e727b9023ebd15d0fabf5b0
guyts <guytsror@gmail.com>
2019-05-06 16:31:12 -0400

    updates to include comparison module for bits of signals

--- transfer-learning/deeplabcut-analysis.py
+++ transfer-learning/deeplabcut-analysis.py
@@ -106,6 +106,7 @@ plot_single_output(output_df)
 
 filename1 = 'animal_3_video_2_150fps_correctDeepCut_resnet50_eyes_onlyJan25shuffle1_350000.csv'
 filename2 = 'march_8_animal_1_video_150fps_correctDeepCut_resnet50_eyes_onlyJan25shuffle1_350000.csv' 
+filename2 = '2019_01_07_animal_1DeepCut_resnet50_blinkingFeb7shuffle1_360000.csv'
 filename2 = '2019_02_07_animal_4DeepCut_resnet50_blinkingFeb7shuffle1_360000.csv' 
 filename2 = '2019_01_14_animal_1DeepCut_resnet50_blinkingFeb7shuffle1_360000.csv'
 filename2 = '2019_01_14_animal_2DeepCut_resnet50_blinkingFeb7shuffle1_360000.csv'
@@ -128,7 +129,7 @@ df.columns = (df.iloc[0] + '_' + df.iloc[1])
 df = df.iloc[2:].reset_index(drop=True)
 df = df.apply(pd.to_numeric)
 
-# Left eye processing:
+# eye processing:
 minor_axis_L, minor_axis_R = minor_length(df)
 
 # plotting (using seaborn)
@@ -143,12 +144,88 @@ d = {'frame': frames,
      }
 output_df = pd.DataFrame(d)
 
+output_df.to_csv(filename2[0:19]+'_dlc.csv',index=False,)
+
 plot_double_output(output_df)
+
+## VALIDATION ANALYSIS FOR SECTIONS OF VIDEOS
+
+mat_output = pd.read_csv('output-files/validation/matfiles/2019_02_07_animal_4_R_SigOutput.csv',header=None)
+mat_validation = pd.read_csv('output-files/validation/valfiles/F7A4R_R_ValOutput.csv',header=None)
+dlc_output = pd.read_csv('output-files/validation/dlcfiles/2019_02_07_animal_4_dlc.csv')
+
+# input the desired start and end time:
+
+begin_time = 61;
+end_time = 63.5;
+
+begin_fr = begin_time*fps
+end_fr = end_time*fps
+'''
+# relevant frames from validation dataset
+begin_fr_val = begin_time*fps*2
+end_fr_val = end_time*fps*2
+# relevant frames from matlab output 
+begin_fr_mat = begin_time*fps
+end_fr_mat = end_time*fps
+# relevant frames from dlc output:
+'''
+# dropping any irrelevant portions from all 3 inputs:
+
+val_data = mat_validation.iloc[begin_fr:]
+dlc_data = dlc_output.iloc[begin_fr:begin_fr+len(val_data)]
+mat_data = mat_output.iloc[begin_fr:begin_fr+len(val_data)]
+
+time = dlc_data.time
+
+# Create a view with all 3 plots using plotly:
+
+trace_dlc = go.Scatter(
+    x = time[0:-1],
+    y = dlc_data.minor_axis_R,
+    mode = 'lines',
+    name = 'ML Approach',
+    marker = dict(
+            color = 'blue'
+            )
+)
+trace_mat = go.Scatter(
+    x = time[0:-1],
+    y = mat_data.iloc[:,0],
+    mode = 'lines',
+    name = 'Active Contour Approach',
+    marker = dict(
+            color = 'brown'
+            )
+)
+trace_val = go.Scatter(
+    x = time[0:-1],
+    y = val_data.iloc[:,0],
+    mode = 'lines',
+    name = 'Manual Validation',
+    marker = dict(
+            color = 'green'
+            )
+)
+
+fig = tools.make_subplots(rows=1, cols=1)
+fig.append_trace(trace_dlc, 1, 1)
+fig.append_trace(trace_mat, 1, 1)
+fig.append_trace(trace_val, 1, 1)
+
+init_notebook_mode(connected=True)
+
+fig['layout'].update(height=800, width=1200, title='Deep Learning vs. Active Contour vs. Manual')
+
+plot(fig)
+
+# calculating score between DLC and Validation:
+
 
 ## IMPORTING OLD DATA TO SUPERIMPOSE FOR COMPARISON
 
-mat_output = pd.read_csv('../final-outputs/an3vid2/animal_3_video_2_L_SigOutput.csv',header=None)
-mat_validation = pd.read_csv('../final-outputs/an3vid2/animal_3_video_2_L_ValOutput.csv',header=None)
+mat_output = pd.read_csv('output-files/validation/matfiles/2019_01_07_animal_1_R_SigOutput.csv',header=None)
+mat_validation = pd.read_csv('output-files/validation/valfiles/J7A1R_R_ValOutput.csv',header=None)
 
 # matching signal lengths by dropping endings of Python generated files:
 
