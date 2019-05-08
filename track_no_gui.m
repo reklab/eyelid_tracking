24cc36383f306b4ea9c66731e26c351efad8c6e1
guyts <guytsror@gmail.com>
2019-05-08 10:54:04 -0400

    update to tracking without exe

--- track_no_gui.m
+++ track_no_gui.m
@@ -1,6 +1,7 @@
 % This code can be used and manipulated to load the process without using
-% the GUI. 
-% Instead, create or edit parameter files in advance with all the required
+% the GUI/exe file
+%
+% Instead, create or edit parameter files (init_param.csv) in advance with all the required
 % parameters, and load using the provided function, to feed into the
 % tracking function, eyelid_tracking_gui.
 
